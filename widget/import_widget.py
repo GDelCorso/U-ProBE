47c1ebc879514f238edd02107cb4c4f42492e324
Lorenzo Bandini <lboa@hotmail.it>
2024-08-01 17:49:54 +0200

    rename some methods

--- widget/import_widget.py
+++ widget/import_widget.py
@@ -42,22 +42,22 @@ class ImportSection:
         self.model_remove_button = ctk.CTkButton(self.file_frame, text="X", command=self.remove_model_file, width=10, height=10, corner_radius=10)
         self.model_remove_button.grid(row=0, column=2, padx=(0, 10), pady=5, sticky="e")
         
-        self.dataloader_file_label = ctk.CTkLabel(self.file_frame, text="No DataSet selected", font=st.TEXT_FONT)
-        self.dataloader_file_label.grid(row=1, column=0, columnspan=2, padx=10, pady=5, sticky="w")
+        self.dataset_file_label = ctk.CTkLabel(self.file_frame, text="No DataSet selected", font=st.TEXT_FONT)
+        self.dataset_file_label.grid(row=1, column=0, columnspan=2, padx=10, pady=5, sticky="w")
 
-        self.dataloader_remove_button = ctk.CTkButton(self.file_frame, text="X", command=self.remove_dataset_file, width=10, height=10, corner_radius=10)
-        self.dataloader_remove_button.grid(row=1, column=2, padx=(0, 10), pady=5, sticky="e")
+        self.dataset_remove_button = ctk.CTkButton(self.file_frame, text="X", command=self.remove_dataset_file, width=10, height=10, corner_radius=10)
+        self.dataset_remove_button.grid(row=1, column=2, padx=(0, 10), pady=5, sticky="e")
         
-        self.dataset_file_label = ctk.CTkLabel(self.file_frame, text="No Data File selected", font=st.TEXT_FONT)
-        self.dataset_file_label.grid(row=2, column=0, columnspan=2, padx=10, pady=5, sticky="w")
+        self.data_file_label = ctk.CTkLabel(self.file_frame, text="No Data File selected", font=st.TEXT_FONT)
+        self.data_file_label.grid(row=2, column=0, columnspan=2, padx=10, pady=5, sticky="w")
 
-        self.dataset_remove_button = ctk.CTkButton(self.file_frame, text="X", command=self.remove_data_file, width=10, height=10, corner_radius=10)
-        self.dataset_remove_button.grid(row=2, column=2, padx=(0, 10), pady=5, sticky="e")
+        self.data_remove_button = ctk.CTkButton(self.file_frame, text="X", command=self.remove_data_file, width=10, height=10, corner_radius=10)
+        self.data_remove_button.grid(row=2, column=2, padx=(0, 10), pady=5, sticky="e")
         
         # Variabili per i percorsi dei file
         self.selected_model_file = None
-        self.dataloader_file_path = None
         self.dataset_file_path = None
+        self.data_file_path = None
 
     def select_model_file(self):
         filetypes = (
@@ -86,8 +86,8 @@ class ImportSection:
         )
         
         if filename:
-            self.dataloader_file_path = filename
-            self.dataloader_file_label.configure(text=f"DataSet: {os.path.basename(filename)}", font=st.TEXT_FONT)
+            self.dataset_file_path = filename
+            self.dataset_file_label.configure(text=f"DataSet: {os.path.basename(filename)}", font=st.TEXT_FONT)
 
     def import_datafile(self):
         filetypes = (
@@ -101,27 +101,28 @@ class ImportSection:
         )
         
         if filename:
-            self.dataset_file_path = filename
-            self.dataset_file_label.configure(text=f"Data File: {os.path.basename(filename)}", font=st.TEXT_FONT)
+            self.data_file_path = filename
+            self.data_file_label.configure(text=f"Data File: {os.path.basename(filename)}", font=st.TEXT_FONT)
     
     def remove_model_file(self):
         self.selected_model_file = None
         self.model_file_label.configure(text="No Model selected", font=st.TEXT_FONT)
         
     def remove_dataset_file(self):
-        self.dataloader_file_path = None
-        self.dataloader_file_label.configure(text="No DataSet selected", font=st.TEXT_FONT)
+        self.dataset_file_path = None
+        self.dataset_file_label.configure(text="No DataSet selected", font=st.TEXT_FONT)
         
     def remove_data_file(self):
-        self.dataset_file_path = None
-        self.dataset_file_label.configure(text="No Data File selected", font=st.TEXT_FONT)
+        self.data_file_path = None
+        self.data_file_label.configure(text="No Data File selected", font=st.TEXT_FONT)
    
     # Metodi per ottenere i percorsi dei file
     def get_model_file(self):
+        print(self.selected_model_file)
         return self.selected_model_file
 
     def get_dataset_file(self):
-        return self.dataloader_file_path
+        return self.dataset_file_path
 
     def get_data_file(self):
-        return self.dataset_file_path
+        return self.data_file_path
